22612a2754b7f20aaf7550e51d3f89a7b81315cb
Abderahman Haouit <abderahman.haouit.2020@mumail.ie>
2022-12-14 18:19:03 +0000

    Updated

--- src/assets/brand/products.js
+++ src/assets/brand/products.js
@@ -1,20 +1,16 @@
 // all images imported from images directory
 import product_01_image_01 from "../images/cc1.jpg";
 import product_01_image_02 from "../images/cc2.jpg";
+import product_01_image_03 from "../images/cd1.jpg";
+import product_01_image_04 from "../images/cd2.jpg";
+import product_01_image_05 from "../images/cd3.jpg";
+import product_01_image_06 from "../images/cck1.jpg";
+import product_01_image_07 from "../images/rc1.jpg";
+import product_01_image_08 from "../images/c1.jpg";
 
-import product_02_image_01 from "../images/cd1.jpg";
-import product_02_image_02 from "../images/cd2.jpg";
-import product_02_image_03 from "../images/cd3.jpg";
-
-import product_03_image_01 from "../images/cck1.jpg";
-
-import product_04_image_01 from "../images/rc1.jpg";
-
-import product_05_image_01 from "../images/c1.jpg";
-
-import product_06_image_01 from "../images/ms1.jpg";
-
-import product_07_image_01 from "../images/sm1.png";
+import product_02_image_01 from "../images/ms1.jpg";
+import product_02_image_02 from "../images/sm1.png";
+import product_02_image_03 from "../images/d1.jpg";
 
 
 const products = [
@@ -29,23 +25,13 @@ const products = [
     desc: "See faqs page for any allergy advice and ingrediants.",
   },
 
-  // {
-  //   id: "02",
-  //   title: "Cheese Cake Tub",
-  //   price: 6,
-  //   image02: product_01_image_02,
-  //   category: "Desserts",
-
-  //   desc: "See faqs page for any allergy advice and ingrediants.",
-  // },
-
   {
     id: "03",
     title: "Cookie Dough Tray",
     price: 7,
-    image01: product_02_image_01,
-    image02: product_02_image_02,
-    image03: product_02_image_03,
+    image01: product_01_image_03,
+    image02: product_01_image_04,
+    image03: product_01_image_05,
     category: "Desserts",
 
     desc: "See faqs page for any allergy advice and ingrediants.",
@@ -55,7 +41,7 @@ const products = [
     id: "04",
     title: "Chocolate Cake Slice",
     price: 5,
-    image01: product_03_image_01,
+    image01: product_01_image_06,
     category: "Desserts",
 
     desc: "See faqs page for any allergy advice and ingrediants.",
@@ -65,7 +51,7 @@ const products = [
     id: "05",
     title: "Red-Velvet Cake Slice",
     price: 5,
-    image01: product_04_image_01,
+    image01: product_01_image_07,
     category: "Desserts",
 
     desc: "See faqs page for any allergy advice and ingrediants.",
@@ -75,7 +61,7 @@ const products = [
     id: "06",
     title: "Croissant",
     price: 3,
-    image01: product_05_image_01,
+    image01: product_01_image_08,
     category: "Desserts",
 
     desc: "See faqs page for any allergy advice and ingrediants.",
@@ -84,7 +70,7 @@ const products = [
     id: "07",
     title: "Biscoff Shake",
     price: 5,
-    image01: product_06_image_01,
+    image01: product_02_image_01,
     category: "Drinks",
 
     desc: "See faqs page for any allergy advice and ingrediants.",
@@ -94,22 +80,21 @@ const products = [
     id: "08",
     title: "Strawberry Smoothie",
     price: 5,
-    image01: product_07_image_01,
+    image01: product_02_image_02,
     category: "Drinks",
 
     desc: "See faqs page for any allergy advice and ingrediants.",
   },
-  // {
-  //   id: "08",
-  //   title: "Thin Cheese Pizza",
-  //   price: 110.0,
-  //   image01: product_03_image_02,
-  //   image02: product_03_image_01,
-  //   image03: product_03_image_03,
-  //   category: "Pizza",
+  
+  {
+    id: "08",
+    title: "Snapple",
+    price: 2.0,
+    image01: product_02_image_03,
+    category: "Drinks",
 
-  //   desc: "See faqs page for any allergy advice and ingrediants.",
-  // },
+    desc: "See faqs page for any allergy advice and ingrediants.",
+  },
 
   // {
   //   id: "09",
