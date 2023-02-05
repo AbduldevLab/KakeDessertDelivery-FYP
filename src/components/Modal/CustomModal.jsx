5a67d292d321af2939da27606ad178e062e96ddc
Abderahman Haouit <abderahman.haouit.2020@mumail.ie>
2023-02-05 04:55:20 +0000

    Updated

--- src/components/Modal/CustomModal.jsx
+++ src/components/Modal/CustomModal.jsx
@@ -1,3 +1,88 @@
+
+import React, { useState } from "react";
+import "../../styles/product-card.css";
+
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+const CustomModal = (props) => {
+  const [toppings, setToppings] = useState([]);
+  const [sauces, setSauces] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addToCart(toppings, sauces);
+    props.closeModal();
+  };
+  return (
+    <Modal
+      show={props.showModal}
+      onHide={props.closeModal}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Select Toppings and Sauces
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="toppings">Toppings:</label>
+            <select
+              id="toppings"
+              // onChange={(e) => setToppings(e.target.value ? e.target.value.split(",") : [])}
+              onChange={(e) => setToppings(e.target.value)}
+            >
+            <option value="">Select a topping</option>
+            <option value="Kinder">Kinder</option>
+            <option value="Caramel fredo">Caramel fredo</option>
+            <option value="Milky bar">Milky bar</option>
+            <option value="Crunchie">Crunchie</option>
+            <option value="M&M">M&M</option>
+            <option value="Flake">Flake</option>
+            <option value="Maltesers">Maltesers</option>
+            <option value="Mint aero">Mint aero</option>
+            <option value="Biscoff bits">Biscoff bits</option>
+            <option value="Oreo bits ">Oreo bits</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="sauces">Sauces:</label>
+            <select
+              id="sauces"
+              // onChange={(e) => setSauces(e.target.value ? e.target.value.split(",") : [])}
+              onChange={(e) => setSauces(e.target.value)}
+            >
+             <option value="">Select a sauce</option>
+             <option value="Milk chocolate">Milk chocolate</option>
+             <option value="White chocolate">White chocolate</option>
+             <option value="Milk chocolate">Kinder</option>
+             <option value="White chocolate">Biscoff</option>
+             <option value="Milk chocolate">Caramel</option>
+             </select>  
+          </div>
+          <Modal.Footer className="modal-footer">
+          <Button type="submit" style={{ backgroundColor: "#CD853F" }}>Add to Cart</Button>
+          <Button onClick={props.closeModal} style={{ backgroundColor: "#CD853F" }}>Cancel</Button>
+          </Modal.Footer>
+        </form>
+      </Modal.Body>
+    </Modal>
+  );
+  
+};
+
+export default CustomModal;
+
+
+
+
+
+
+
 // import React from "react";
 
 // import "../../styles/product-card.css";
@@ -65,62 +150,3 @@
 // export default EditProduct;
 /////////////////////////////////////////////////////////////////////////
 // ///////////////////////////////////////////////////
-
-
-import React, { useState } from "react";
-import "../../styles/product-card.css";
-
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-
-const CustomModal = (props) => {
-  const [toppings, setToppings] = useState([]);
-  const [sauces, setSauces] = useState([]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.addToCart(toppings, sauces);
-    props.closeModal();
-  };
-  console.log("modalOpen:", props.modalOpen);
-  return (
-    <Modal
-      show={props.showModal}
-      onHide={props.closeModal}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Select Toppings and Sauces
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="toppings">Toppings:</label>
-            <input
-              type="text"
-              id="toppings"
-              onChange={(e) => setToppings(e.target.value ? e.target.value.split(",") : [])}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="sauces">Sauces:</label>
-            <input
-              type="text"
-              id="sauces"
-              onChange={(e) => setSauces(e.target.value ? e.target.value.split(",") : [])}
-            />
-          </div>
-          <Button type="submit">Add to Cart</Button>
-          <Button onClick={props.closeModal}>Cancel</Button>
-        </form>
-      </Modal.Body>
-    </Modal>
-  );
-  
-};
-
-export default CustomModal;
--- src/components/UI/product-card/ProductCard.jsx
+++ src/components/UI/product-card/ProductCard.jsx
@@ -1,3 +1,66 @@
+import React, { useState } from "react";
+
+import "../../../styles/product-card.css";
+import ReactModal from "../../Modal/CustomModal";
+
+import { Link } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../../store/shopping-cart/cartSlice";
+
+const ProductCard = (props) => {
+  const { id, title, image01, price } = props.item;
+  const dispatch = useDispatch();
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const addToCart = (toppings, sauces) => {
+    dispatch(
+      cartActions.addItem({
+        id,
+        title,
+        image01,
+        price,
+        toppings,
+        sauces,
+      })
+    );
+  };
+
+  return (
+    <div className="product__item">
+      <div className="product__img">
+        <img src={image01} alt="product-img" className="w-50" />
+      </div>
+
+      <div className="product__content">
+        <h5>
+          <Link to={`/foods/€{id}`}>{title}</Link>
+        </h5>
+        <div className=" d-flex align-items-center justify-content-between ">
+          <span className="product__price">€{price}</span>
+          <button className="addTOCart__btn" onClick={() => {
+           setModalOpen(true)}}>
+            Add to Cart
+          </button>
+        </div>
+      </div>
+      {modalOpen && (
+        <ReactModal
+          showModal={modalOpen}
+          closeModal={() => setModalOpen(false)}
+          addToCart={addToCart}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ProductCard;
+
+
+
+
 // import React from "react";
 
 // import "../../../styles/product-card.css";
@@ -50,64 +113,3 @@
 
 // export default ProductCard;
 //////////////////////////////////////////////////////
-
-import React, { useState } from "react";
-
-import "../../../styles/product-card.css";
-import ReactModal from "../../Modal/CustomModal";
-
-import { Link } from "react-router-dom";
-
-import { useDispatch } from "react-redux";
-import { cartActions } from "../../../store/shopping-cart/cartSlice";
-
-const ProductCard = (props) => {
-  const { id, title, image01, price } = props.item;
-  const dispatch = useDispatch();
-
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const addToCart = (toppings, sauces) => {
-    dispatch(
-      cartActions.addItem({
-        id,
-        title,
-        image01,
-        price,
-        toppings,
-        sauces,
-      })
-    );
-  };
-
-  return (
-    <div className="product__item">
-      <div className="product__img">
-        <img src={image01} alt="product-img" className="w-50" />
-      </div>
-
-      <div className="product__content">
-        <h5>
-          <Link to={`/foods/€{id}`}>{title}</Link>
-        </h5>
-        <div className=" d-flex align-items-center justify-content-between ">
-          <span className="product__price">€{price}</span>
-          <button className="addTOCart__btn" onClick={() => {
-            console.log("add to cart clicked");
-           setModalOpen(true)}}>
-            Add to Cart
-          </button>
-        </div>
-      </div>
-      {modalOpen && (
-        <ReactModal
-          showModal={modalOpen}
-          closeModal={() => setModalOpen(false)}
-          addToCart={addToCart}
-        />
-      )}
-    </div>
-  );
-};
-
-export default ProductCard;
