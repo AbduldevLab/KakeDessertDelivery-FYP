c521eda5f69cc79e4fc31da4e75402e45178ae4d
Abderahman Haouit <abderahman.haouit.2020@mumail.ie>
2023-02-27 13:09:13 +0000

    updated

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,12 +1,12 @@
-import React from "react";
+// import React from "react";
 
-const Helmet = (props) => {
-  const title = props.title.split(" - ")[0]; // Remove " - Home" from title
-  document.title = "Kake Dessert Delivery - " + title;
-  return <div className="w-100">{props.children}</div>;
-};
+// const Helmet = (props) => {
+//   const title = props.title.split(" - ")[0]; // Remove " - Home" from title
+//   document.title = "Kake Dessert Delivery - " + title;
+//   return <div className="w-100">{props.children}</div>;
+// };
 
-export default Helmet;
+// export default Helmet;
 
 
 // import React, { useState } from "react";
@@ -18,11 +18,11 @@ export default Helmet;
 //     setShowTitle(true);
 //   };
 
-//   document.title = "Kake Dessert Delivery" + (showTitle ? " - " + props.title : "");
+//   document.title = "Kake Dessert Delivery - Grab your kake today!" + (showTitle ? " - " + props.title : "");
   
 //   return (
 //     <div className="w-100">
-//       <a href="#" onClick={handleClick}>Link/Site</a>
+//       <a href="home" onClick={handleClick}></a>
 //       {showTitle && <span> - {props.title}</span>}
 //       {props.children}
 //     </div>
@@ -30,3 +30,45 @@ export default Helmet;
 // };
 
 // export default Helmet;
+
+import React, { useState, useEffect } from "react";
+
+const Helmet = (props) => {
+  const [showTitle, setShowTitle] = useState(false);
+
+  const handleClick = () => {
+    setShowTitle(true);
+    window.history.pushState(null, "", "/home"); // update URL when navigating to new page
+  };
+
+  useEffect(() => {
+    const handlePopState = () => {
+      if (window.location.pathname === "/") {
+        setShowTitle(false);
+      }
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
+
+  document.title =
+    "Kake Dessert Delivery - Grab your kake today!" +
+    (showTitle ? " - " + props.title : "");
+
+  return (
+    <div className="w-100">
+      <a href="/" onClick={handleClick}>
+        Home
+      </a>
+      {showTitle && <span> - {props.title}</span>}
+      {props.children}
+    </div>
+  );
+};
+
+export default Helmet;
+
