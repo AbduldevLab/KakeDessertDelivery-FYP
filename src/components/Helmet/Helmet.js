ae5a0bc1e1aac614467b4fa1140f41bda639ef9d
Abderahman Haouit <abderahman.haouit.2020@mumail.ie>
2023-02-27 00:15:37 +0000

    Updated

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -7,3 +7,26 @@ const Helmet = (props) => {
 };
 
 export default Helmet;
+
+
+// import React, { useState } from "react";
+
+// const Helmet = (props) => {
+//   const [showTitle, setShowTitle] = useState(false);
+
+//   const handleClick = () => {
+//     setShowTitle(true);
+//   };
+
+//   document.title = "Kake Dessert Delivery" + (showTitle ? " - " + props.title : "");
+  
+//   return (
+//     <div className="w-100">
+//       <a href="#" onClick={handleClick}>Link/Site</a>
+//       {showTitle && <span> - {props.title}</span>}
+//       {props.children}
+//     </div>
+//   );
+// };
+
+// export default Helmet;
