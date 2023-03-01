579b5af5706c4c1e349ed02c2ac1272a646efba8
Abderahman Haouit <abderahman.haouit.2020@mumail.ie>
2023-03-01 01:20:31 +0000

    Updated

--- src/components/AdminDash/menuContext.js
+++ src/components/AdminDash/menuContext.js
@@ -1,20 +1,20 @@
-import React, { createContext, useState, useContext } from "react";
-const MenuContext = createContext()
+// import React, { createContext, useState, useContext } from "react";
+// const MenuContext = createContext()
 
-export const useMenuContext = () => {
-    return useContext(MenuContext);
-}
-export const AppMenuContext = ({ children }) => {
-    const [isMenuToggled, setToggleMenu] = useState(true)
-    const toggleMenu = () => {
-        isMenuToggled ? setToggleMenu(false) : setToggleMenu(true)
-    }
-    return (
-        <MenuContext.Provider value={{ isMenuToggled, toggleMenu }}>
-            <div style={{ position: "relative", width: "100%" }}>
-                {children}
-            </div>
+// export const useMenuContext = () => {
+//     return useContext(MenuContext);
+// }
+// export const AppMenuContext = ({ children }) => {
+//     const [isMenuToggled, setToggleMenu] = useState(true)
+//     const toggleMenu = () => {
+//         isMenuToggled ? setToggleMenu(false) : setToggleMenu(true)
+//     }
+//     return (
+//         <MenuContext.Provider value={{ isMenuToggled, toggleMenu }}>
+//             <div style={{ position: "relative", width: "100%" }}>
+//                 {children}
+//             </div>
 
-        </MenuContext.Provider>
-    )
-}
+//         </MenuContext.Provider>
+//     )
+// }
