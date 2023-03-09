b8f359328ea3564bb220a930a7a1371c422b25ee
Abderahman Haouit <abderahman.haouit.2020@mumail.ie>
2023-03-09 23:57:54 +0000

    updated

--- src/pages/AdminDash/Dashboard.jsx
+++ src/pages/AdminDash/Dashboard.jsx
@@ -1,9 +1,12 @@
 // This is the dashboard page for the admin panel
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/AdminDash/panel.css";
 
 //This is used to import the react-router-dom components
 import { useNavigate } from "react-router-dom";
+
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebase.js";
 
 //This is used to import the reactstrap components
 const Dashboard = () => {
@@ -11,6 +14,8 @@ const Dashboard = () => {
   //This is used to navigate to the admin page
   const navigate = useNavigate();
 
+  const [numOrders, setNumOrders] = useState(0); //This is used to store the number of orders
+  const [numUsers, setNumUsers] = useState(0); //This is used to store the number of users
   //This is used to check if the user is authenticated
   useEffect(() => {
     // Check if the user is authenticated
@@ -18,6 +23,15 @@ const Dashboard = () => {
     if (!isAuthenticated) { // If the user is not authenticated
       navigate("/admin"); // Navigate to the admin page
     }
+
+     // Fetch the number of orders
+     const fetchNumOrders = async () => {
+      const querySnapshot = await getDocs(collection(db, "Orders")); //This is used to get the orders from the database
+      const querySnapshot1 = await getDocs(collection(db, "Users")); //This is used to get the users from the database
+      setNumOrders(querySnapshot.size); //This is used to set the number of orders
+      setNumUsers(querySnapshot1.size); //This is used to set the number of users
+    };
+    fetchNumOrders();
   }, [navigate]);// eslint-disable-line react-hooks/exhaustive-deps
 
   //This is used to display the dashboard page
@@ -64,7 +78,7 @@ const Dashboard = () => {
                   </div>
                   <div className="card-widget-body">
                     {/* //This is used to display the number of users */}
-                    <h1 className="widget-count">0</h1>
+                    <h1 className="widget-count">{numOrders}</h1>
                     <p className="widget-name">Users</p>
                   </div>
                 </div>
@@ -78,7 +92,7 @@ const Dashboard = () => {
                   </div>
                   <div className="card-widget-body">
                     {/* //This is used to display the number of orders */}
-                    <h1 className="widget-count">0</h1>
+                    <h1 className="widget-count">{numUsers}</h1>
                     <p className="widget-name">Orders Received</p>
                   </div>
                 </div>
