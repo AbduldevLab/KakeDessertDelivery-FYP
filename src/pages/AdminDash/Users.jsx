59e331ca383bd3123135afc9c5002a3a39a96982
Abderahman Haouit <abderahman.haouit.2020@mumail.ie>
2023-03-15 18:53:49 +0000

    Updated

--- src/pages/AdminDash/Users.jsx
+++ src/pages/AdminDash/Users.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"; //This is used to import the react-router-dom components
 import { makeStyles } from "@material-ui/core/styles"; //This is used to import the material-ui components
+//This is used to import the material-ui components
 import {
   Table,
   TableBody,
@@ -12,6 +13,7 @@ import {
   Typography,
 } from "@material-ui/core"; //This is the table component from material-ui
 import { db } from "../../config/firebase.js"; //This is used to import the firebase database
+//This is used to import the firebase firestore components
 import {
   collection,
   query,
@@ -32,6 +34,7 @@ const useStyles = makeStyles({
   tableContainer: {
     overflowX: "auto",
   },
+  //This is used to style the coupon code banner
   couponBanner: {
     backgroundColor: "#CD853F",
     color: "#000",
@@ -59,9 +62,22 @@ const Users = () => {
   const classes = useStyles();
   const [users, setUsers] = useState([]); //This is used to store the users
   const [lastUserTimestamp, setLastUserTimestamp] = useState(null); //This is used to store the last user timestamp
+  const [tooltipOpen, setTooltipOpen] = useState(false); // add a state to keep track of tooltip open status
+
+  //This is used to handle the tooltip close
+  const handleTooltipClose = () => {
+    setTooltipOpen(false);
+  };
+  //This is used to handle the tooltip open
+  const handleTooltipOpen = () => {
+    setTooltipOpen(true);
+    setTimeout(() => {
+      setTooltipOpen(false);
+    }, 3000); // set the time in milliseconds after which the tooltip should disappear
+  };
   
   //This is used to get the users from the database
-  useEffect(() => {
+  useEffect(() => {//This is used to get the users from the database
     let q = query(
       collection(db, "Users"),
       where(
@@ -72,7 +88,7 @@ const Users = () => {
       orderBy("signupTime", "desc")
     );
     if (lastUserTimestamp) {
-      q = query(q, startAfter(lastUserTimestamp));
+      q = query(q, startAfter(lastUserTimestamp));//This is used to get the users from the database signed up after the last user timestamp
     }
 
     //This is used to get the users from the database using snapshot
@@ -81,27 +97,27 @@ const Users = () => {
 
       //This is used to get the users from the database and filtering them based on timestamp only fetch the users signed up after the last user timestamp
       querySnapshot.forEach((doc) => {
-        const { name, email, signupTime } = doc.data();
-        users.push({ name, email, signupTime });
+        const { name, email, signupTime } = doc.data(); //This is used to get the user data
+        users.push({ name, email, signupTime }); //This is used to push the user data to the users array
         if (
           !lastUserTimestamp ||
-          signupTime.toMillis() > lastUserTimestamp.toMillis()
+          signupTime.toMillis() > lastUserTimestamp.toMillis() //This is used to check if the user timestamp is greater than the last user timestamp
         ) {
-          setLastUserTimestamp(signupTime);
+          setLastUserTimestamp(signupTime);//This is used to set the last user timestamp
         }
       });
 
       //This is used to update the users
       setUsers((existingUsers) => {
-        const updatedUsers = [...existingUsers];
-        users.forEach((user) => {
+        const updatedUsers = [...existingUsers]; 
+        users.forEach((user) => { //This is used to update the users if they already exist
           const existingUserIndex = existingUsers.findIndex(
-            (u) => u.email === user.email
+            (u) => u.email === user.email //This is used to check if the user already exists
           );
-          if (existingUserIndex !== -1) {
-            updatedUsers[existingUserIndex] = user;
+          if (existingUserIndex !== -1) { //This is used to update the user if they already exist
+            updatedUsers[existingUserIndex] = user; 
           } else {
-            updatedUsers.push(user);
+            updatedUsers.push(user); //This is used to add the user if they do not exist
           }
         });
         return updatedUsers;
@@ -121,14 +137,18 @@ const Users = () => {
     <div className="admin-content">
       <div className="admin-wrapper">
       <Tooltip
-          title="Use coupon code KAKE10 for 10% discount on all products!"
+          title="Send email coupon code KAKE10 for 10% discount on all products! to the users below"
           arrow
+          open={tooltipOpen} // pass the state to the open prop
+          onClose={handleTooltipClose} // pass the function to the onClose prop
+          onClick={handleTooltipOpen} // pass the function to the onClick prop
         >
-          <Typography variant="h6" className={classes.couponBanner}>
+          {/* //This is used to display the coupon code */}
+          <Typography variant="h6" className={classes.couponBanner} >
             Current Coupon Code: KAKE10
           </Typography>
         </Tooltip>
-        <TableContainer component={Paper}>
+        <TableContainer component={Paper}> {/**This is used to display the table */}
           <Table className={classes.table} aria-label="users table">
             <TableHead>
               <TableRow>
@@ -145,13 +165,13 @@ const Users = () => {
                 (
                   user //This is used to match the users with the table
                 ) => (
-                  <TableRow key={user.name}>
+                  <TableRow key={user.name}> {/**This is used to display the users in the table */}
                     <TableCell component="th" scope="row">
-                      {user.name}
+                      {user.name} {/**This is used to display the user name */}
                     </TableCell>
-                    <TableCell align="left">{user.email}</TableCell>
+                    <TableCell align="left">{user.email}</TableCell> {/**This is used to display the user email */}
                     <TableCell align="left">
-                      {user.signupTime?.toDate().toLocaleString()}
+                      {user.signupTime?.toDate().toLocaleString()} {/**This is used to display the user signup time */}
                     </TableCell>
                   </TableRow>
                 )
