1a0e35038903b661283629aab4b64890249ac6cd
Abderahman Haouit <abderahman.haouit.2020@mumail.ie>
2023-03-25 16:35:15 +0000

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -208,7 +208,7 @@ const Home = () => {
         <Container>
           <Row>
             <Col lg="12" className="text-center">
-              <h2>Our Popular Choices</h2>
+              <h2>Our Most-loved Favorites</h2>
             </Col>
 
 {/* // This is the map function that maps through the popularData array */}
